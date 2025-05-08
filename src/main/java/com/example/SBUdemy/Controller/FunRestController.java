93569fbdf33abc0d5960c2a3f221e02975e6022b
thangbodoiqua <dluy2406@gmail.com>
2025-05-08 11:05:13 +0700

    8/5

---
+++ src/main/java/com/example/SBUdemy/Controller/FunRestController.java
@@ -0,0 +1,24 @@
+package com.example.SBUdemy.Controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class FunRestController {
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World " + coachName + ' ' + teamName;
+    }
+    @GetMapping("/workout")
+    public String work(){
+        return "Im working out";
+    }
+}
