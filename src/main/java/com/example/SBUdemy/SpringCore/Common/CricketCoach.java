5b7958069397ffa3725c49a0c6fd7147e37d6fff
Thangbodoiqua <dluy2406@gmail.com>
2025-05-13 10:36:25 +0700

    Scope

--- src/main/java/com/example/SBUdemy/SpringCore/Common/CricketCoach.java
+++ src/main/java/com/example/SBUdemy/SpringCore/Common/CricketCoach.java
@@ -1,9 +1,12 @@
 package com.example.SBUdemy.SpringCore.Common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach{
     public CricketCoach() {
         System.out.println("In Constructor: " + getClass().getSimpleName());
