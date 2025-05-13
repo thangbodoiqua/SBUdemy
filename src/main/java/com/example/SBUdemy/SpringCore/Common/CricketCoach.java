0f0c0b5f66c190d09fcf5991725cc32ce8292085
Thangbodoiqua <dluy2406@gmail.com>
2025-05-13 09:25:51 +0700

    Primary

--- src/main/java/com/example/SBUdemy/SpringCore/Common/CricketCoach.java
+++ src/main/java/com/example/SBUdemy/SpringCore/Common/CricketCoach.java
@@ -1,8 +1,10 @@
 package com.example.SBUdemy.SpringCore.Common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class CricketCoach implements Coach{
     @Override
     public String getDailyWorkout(){
