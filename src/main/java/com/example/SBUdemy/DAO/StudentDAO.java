81fa04f14f7e6244f75e9500128a1a65ce20882d
Thangbodoiqua <dluy2406@gmail.com>
2025-05-26 10:41:31 +0700

    Saving Java Object

---
+++ src/main/java/com/example/SBUdemy/DAO/StudentDAO.java
@@ -0,0 +1,6 @@
+package com.example.SBUdemy.DAO;
+import com.example.SBUdemy.entity.Student;
+
+public interface StudentDAO {
+    void save(Student theStudent);
+}
--- src/main/java/com/example/SBUdemy/SbUdemyApplication.java
+++ src/main/java/com/example/SBUdemy/SbUdemyApplication.java
@@ -1,5 +1,7 @@
 package com.example.SBUdemy;
 
+import com.example.SBUdemy.DAO.StudentDAO;
+import com.example.SBUdemy.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,12 +16,34 @@ public class SbUdemyApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner() {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello World!");
+			createMultipleStudent(studentDAO);
 		};
 	}
 
+	private void createStudent(StudentDAO studentDAO) {
+		System.out.println("Creating new student");
+		Student tempStudent = new Student("Smith", "John", "JohnSmith@gmail.com");
 
+		System.out.println("Saving new student");
+		studentDAO.save(tempStudent);
+
+		System.out.println("Saved new student with id: " + tempStudent.getId());
+	}
+
+	private void createMultipleStudent(StudentDAO studentDAO) {
+		System.out.println("Creating students");
+		Student tempStudent1 = new Student("Dinh", "Thang", "thang@gmail.com");
+		Student tempStudent2 = new Student("Ngoc", "Dac", "dac@gmail.com");
+		Student tempStudent3 = new Student("Cong", "Doan", "doan@gmail.com");
+
+		System.out.println("Saving students");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+
+		System.out.println("Saved multiple students");
+	}
 
 }
