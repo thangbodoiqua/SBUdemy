9ed55bcce0e6291a3c20e621f55a996376384695
Thangbodoiqua <dluy2406@gmail.com>
2025-06-02 10:51:17 +0700

    Path Variables

--- src/main/java/com/example/SBUdemy/rest/StudentRestController.java
+++ src/main/java/com/example/SBUdemy/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.example.SBUdemy.rest;
 
 import com.example.SBUdemy.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +13,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    @GetMapping("/students")
-    public List<Student> getStudent() {
-        List<Student> theStudents = new ArrayList<>();
+    private List<Student> theStudents;
+
+    @PostConstruct
+    public void loadData() {
+         theStudents = new ArrayList<>();
         theStudents.add(new Student("Poornima", "Patel"));
         theStudents.add(new Student("Mario", "Rossi"));
         theStudents.add(new Student("Mary", "Smith"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudent() {
         return theStudents;
+    }
+
+    @GetMapping("/student/{studentId}")
+    public Student getStudentById(@PathVariable int studentId) {
+        return theStudents.get(studentId);
     }
 }
