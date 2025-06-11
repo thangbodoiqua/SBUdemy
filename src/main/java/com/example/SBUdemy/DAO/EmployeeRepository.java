5965e603e0b0ab85de606c144a08a102a48d58a4
Thangbodoiqua <dluy2406@gmail.com>
2025-06-11 10:44:36 +0700

    Spring Data Rest configuration

--- src/main/java/com/example/SBUdemy/DAO/EmployeeRepository.java
+++ src/main/java/com/example/SBUdemy/DAO/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.example.SBUdemy.DAO;
 
 import com.example.SBUdemy.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
