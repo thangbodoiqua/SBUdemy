bb443a6abecb74d3a41bd928112225a17db4bad3
Thangbodoiqua <dluy2406@gmail.com>
2025-06-15 08:39:19 +0700

    Spring MVC with Thymeleaf

--- src/main/java/com/example/SBUdemy/DAO/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.SBUdemy.DAO;
-
-import com.example.SBUdemy.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
