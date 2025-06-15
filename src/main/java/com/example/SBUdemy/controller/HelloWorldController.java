05cf5aace6b1c6359c81e15ca961d3097f02a420
Thangbodoiqua <dluy2406@gmail.com>
2025-06-15 10:24:48 +0700

    Add Data to Spring MVC Model

--- src/main/java/com/example/SBUdemy/controller/HelloWorldController.java
+++ src/main/java/com/example/SBUdemy/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.example.SBUdemy.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,6 +13,15 @@ public class HelloWorldController {
     }
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+        String theName = request.getParameter("studentName");
+        theName = theName.toUpperCase();
+        String result = "Yo! " + theName;
+        model.addAttribute("message",result);
+
         return "helloworld";
     }
 
