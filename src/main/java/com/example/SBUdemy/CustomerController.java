c8d8f681cecb7d6543eedb80ffdbe782fcdc8560
Thangbodoiqua <dluy2406@gmail.com>
2025-06-18 09:18:45 +0700

    Spring MVC Validation @InitBinder

--- src/main/java/com/example/SBUdemy/CustomerController.java
+++ src/main/java/com/example/SBUdemy/CustomerController.java
@@ -1,16 +1,24 @@
 package com.example.SBUdemy;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
 
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
     @GetMapping("/")
     public String showForm(Model theModel) {
         theModel.addAttribute("customer", new Customer());
@@ -22,9 +30,11 @@ public class CustomerController {
             @Valid @ModelAttribute("customer") Customer theCustomer,
             BindingResult bindingResult
     ) {
+        System.out.println("last name: |" + theCustomer.getLastName() + "|");
         if (bindingResult.hasErrors())
             return "customer-form";
         else
             return "customer-confirmation";
     }
+
 }
