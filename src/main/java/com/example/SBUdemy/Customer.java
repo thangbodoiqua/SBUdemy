bc81fc1940f5c3c7fdeb0e92a63835c464564773
Thangbodoiqua <dluy2406@gmail.com>
2025-06-18 09:32:32 +0700

    Spring MVC Validation Number Range and Regex

--- src/main/java/com/example/SBUdemy/Customer.java
+++ src/main/java/com/example/SBUdemy/Customer.java
@@ -1,7 +1,6 @@
 package com.example.SBUdemy;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -10,6 +9,13 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min=1, message = "is required")
     private String lastName="";
+
+    @Min(value=0, message="must be greater or equal to 0")
+    @Max(value=10, message="must be less or equal to 10")
+    private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -25,5 +31,21 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
