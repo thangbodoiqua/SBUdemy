3a8b3b3bdf40932381d6a87bfd14729311664109
Thangbodoiqua <dluy2406@gmail.com>
2025-06-23 20:50:27 +0700

    Setup For CRUD DBS project 228

---
+++ src/main/java/com/example/SBUdemy/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.SBUdemy.dao;
+
+import com.example.SBUdemy.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+}
