0a2c65537cdbe6cc084db113d7eae57a0a42c6c5
thangbodoiqua <dluy2406@gmail.com>
2025-06-24 13:41:42 +0700

    CRUD DBS Project Delete Employee 239-240

--- src/main/java/com/example/SBUdemy/controller/EmployeeController.java
+++ src/main/java/com/example/SBUdemy/controller/EmployeeController.java
@@ -49,4 +49,10 @@ public class EmployeeController {
 
         return "employees/employee-form";
     }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int theId) {
+        employeeService.deleteById(theId);
+        return "redirect:/employees/list";
+    }
 }
