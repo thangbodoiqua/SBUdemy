db45f6a52966209a80ee64296d42ba68917fc54b
Thangbodoiqua <dluy2406@gmail.com>
2025-06-26 10:40:11 +0700

    Sp MVC Security Custom Denied Page - 267-268

--- src/main/java/com/example/SBUdemy/security/DemoSecurityConfig.java
+++ src/main/java/com/example/SBUdemy/security/DemoSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.SBUdemy.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +12,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
+    private static final Logger log = LoggerFactory.getLogger(DemoSecurityConfig.class);
+
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
 
@@ -53,7 +57,9 @@ public class DemoSecurityConfig {
                 )
                 .logout(logout -> logout
                         .permitAll()
-                );
+                )
+                .exceptionHandling(configurer -> configurer
+                        .accessDeniedPage("/access-denied"));
 
         return  http.build();
     }
