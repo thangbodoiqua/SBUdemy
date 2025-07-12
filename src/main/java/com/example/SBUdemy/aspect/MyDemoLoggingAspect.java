b44610b8bfbc20616886d5d5608432276ff8e569
Thangbodoiqua <dluy2406@gmail.com>
2025-07-12 15:12:29 +0700

    AOP - Pointcut Declarations - 368 - 370

--- src/main/java/com/example/SBUdemy/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/example/SBUdemy/aspect/MyDemoLoggingAspect.java
@@ -2,17 +2,25 @@ package com.example.SBUdemy.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
+    @Pointcut("execution( * com.example.SBUdemy.dao.*.*(..))")
+    private void forDAOPackage() {};
+
 //    @Before("execution(public void addAccount())")
-    @Before("execution( * com.example.SBUdemy.dao.*.*(..))")
+    @Before("forDAOPackage()")
     public void beforeAddAccountAdvice() {
-
         System.out.println("\n==> Executing @Before advice on method");
-
     }
 
+    @Before("forDAOPackage()")
+    public void performApiAnalytics() {
+
+        System.out.println("\n==> Performing API analytics");
+    }
 }
